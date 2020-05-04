422c2e7d87cd63f9506fafcc2c3efa939d830ab3
tomblong <haniffauzan200015@gmail.com>
2020-05-04 13:59:24 +0700

    Selesai

--- app/src/main/java/com/aplikasi/aplikasiservice/MainActivity.java
+++ app/src/main/java/com/aplikasi/aplikasiservice/MainActivity.java
@@ -12,12 +12,12 @@ import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSetBtn;
-    private Button mUnsetBtn;
+    private  Button mUnsetBtn;
     private RadioButton mMenitRadio;
     private RadioButton mLimaRadio;
     private RadioButton mTigaPuluhRadio;
     private RadioButton mJamRadio;
-    private RadioButton mTimeRadioGroup;
+    private RadioGroup mTimeRadioGroup;
     public int mChangeTime = 60;
 
     @Override
@@ -25,15 +25,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSetBtn = (Button)findViewById(R.id.btnSet);
-        mUnsetBtn = (Button)findViewById(R.id.btnUnset);
-        mMenitRadio = (RadioButton)findViewById(R.id.radio0);
-        mLimaRadio = (RadioButton)findViewById(R.id.radio1);
-        mTigaPuluhRadio = (RadioButton)findViewById(R.id.radio2);
-        mJamRadio = (RadioButton)findViewById(R.id.radio3);
-        mTimeRadioGroup = (RadioButton)findViewById(R.id.radioGroup);
+        mSetBtn = (Button) findViewById(R.id.btnSet);
+        mUnsetBtn = (Button) findViewById(R.id.btnUnset);
+        mMenitRadio = (RadioButton) findViewById(R.id.radio0);
+        mLimaRadio = (RadioButton) findViewById(R.id.radio1);
+        mTigaPuluhRadio = (RadioButton) findViewById(R.id.radio2);
+        mJamRadio = (RadioButton) findViewById(R.id.radio3);
+        mTimeRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
 
-        mUnsetBtn.setOnClickListener(new OnClickListener(){
+        mUnsetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mDisable = new Intent(MainActivity.this,
@@ -42,28 +42,22 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-        mSetBtn.setOnClickListener(new View.OnClickListener(){
-
+        mSetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int mRadioID = mTimeRadioGroup.getCheckedRadioButtonId();
-                if (mMenitRadio.getId()==mRadioID){mChangeTime=60;}
+                if(mMenitRadio.getId()==mRadioID){mChangeTime=60;}
                 else if (mLimaRadio.getId()==mRadioID){mChangeTime=5*60;}
                 else if (mTigaPuluhRadio.getId()==mRadioID){mChangeTime=30*60;}
                 else if (mJamRadio.getId()==mRadioID){mChangeTime=60*60;}
 
-                Intent mService = new Intent(MainActivity.this, WallpaperChangeService.class);
-                /* membuat bundle dan menyimpan pasangan nilai dengan kuncinya. */
+                Intent mService = new Intent(MainActivity.this,
+                        WallpaperChangeService.class);
                 Bundle mBundleTime = new Bundle();
                 mBundleTime.putInt("durasi", mChangeTime);
-                /* Menaruh bundle kedalam intent */
                 mService.putExtras(mBundleTime);
-                /* Memulai Service */
                 startService(mService);
-                /* mengakhiri avtivyty */
-                finish();
             }
         });
-
     }
 }
